3e208aa8f1cc4c950ad9471ec6a40e6c780f3af6
Jesper Särnesjö <jesper@sarnesjo.org>
2014-11-29 12:40:14 +1100

    Move input handling to InputHandler.

--- core/src/main/scala/hack/HackGame.scala
+++ core/src/main/scala/hack/HackGame.scala
@@ -1,7 +1,5 @@
 package hack
 
-import com.badlogic.gdx.Gdx.{app, input}
-import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.{Game, Screen}
 import hack.game.{Arch, Tile, World}
 
@@ -31,41 +29,6 @@ class GameScreen extends Screen {
     w
   }
 
-  def handleGlobalInput(): Unit = {
-    if (input.isKeyJustPressed(Keys.ESCAPE))
-      app.exit()
-  }
-
-  def keyboardInput(keys: PlayerKeys): PlayerInput = {
-    var x = 0: Int
-    var y = 0: Int
-    var t = 0: Int
-    var a = false: Boolean
-
-    if (input.isKeyJustPressed(keys.left))
-      x -= 1
-
-    if (input.isKeyJustPressed(keys.right))
-      x += 1
-
-    if (input.isKeyJustPressed(keys.down))
-      y -= 1
-
-    if (input.isKeyJustPressed(keys.up))
-      y += 1
-
-    if (input.isKeyJustPressed(keys.previous))
-      t -= 1
-
-    if (input.isKeyJustPressed(keys.next))
-      t += 1
-
-    if (input.isKeyJustPressed(keys.action))
-      a = true
-
-    PlayerInput(x, y, t, a)
-  }
-
   def show() = {}
 
   def hide() = {}
@@ -91,10 +54,6 @@ class GameScreen extends Screen {
       world.tick += 1
       simulationAccu -= simulationTickEvery
     }
-
-    handleGlobalInput()
-    val input1: PlayerInput = keyboardInput(PlayerKeys.Player1)
-    val input2: PlayerInput = keyboardInput(PlayerKeys.Player2)
 
     renderer.render(world, simulationAccu)
   }
--- core/src/main/scala/hack/InputHandler.scala
+++ core/src/main/scala/hack/InputHandler.scala
@@ -1,13 +1,50 @@
 package hack
 
+import com.badlogic.gdx.Gdx.{app, input}
+import com.badlogic.gdx.Input.Keys
 import hack.game.World
 
-/**
- * Created by michael on 29/11/14.
- */
 class InputHandler {
-  def handleFor(world:World) {
-    // read raw from Gdx stuff
+  def handleGlobalInput(): Unit = {
+    if (input.isKeyJustPressed(Keys.ESCAPE))
+      app.exit()
   }
 
+  def keyboardInput(keys: PlayerKeys): PlayerInput = {
+    var x = 0: Int
+    var y = 0: Int
+    var t = 0: Int
+    var a = false: Boolean
+
+    if (input.isKeyJustPressed(keys.left))
+      x -= 1
+
+    if (input.isKeyJustPressed(keys.right))
+      x += 1
+
+    if (input.isKeyJustPressed(keys.down))
+      y -= 1
+
+    if (input.isKeyJustPressed(keys.up))
+      y += 1
+
+    if (input.isKeyJustPressed(keys.previous))
+      t -= 1
+
+    if (input.isKeyJustPressed(keys.next))
+      t += 1
+
+    if (input.isKeyJustPressed(keys.action))
+      a = true
+
+    PlayerInput(x, y, t, a)
+  }
+
+  def handleFor(world: World) {
+    handleGlobalInput()
+    val input1: PlayerInput = keyboardInput(PlayerKeys.Player1)
+    val input2: PlayerInput = keyboardInput(PlayerKeys.Player2)
+
+    // TODO: apply to world
+  }
 }
