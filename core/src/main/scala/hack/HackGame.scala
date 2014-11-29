a4a5e49296014261eb2cee364bf8bf96b2002cbc
Jesper Särnesjö <jesper@sarnesjo.org>
2014-11-29 11:41:09 +1100

    Format HackGame.

--- core/src/main/scala/hack/HackGame.scala
+++ core/src/main/scala/hack/HackGame.scala
@@ -1,10 +1,6 @@
 package hack
 
-/**
- * Created by michael on 29/11/14.
- */
-
-import com.badlogic.gdx.{Screen, Game}
+import com.badlogic.gdx.{Game, Screen}
 import hack.game.{Tile, World}
 
 class HackGame extends Game {
@@ -21,7 +17,7 @@ class GameScreen extends Screen {
   val world = generateWorld()
   val renderer = new Renderer()
 
-  def generateWorld() : World = {
+  def generateWorld(): World = {
     val w = new World(16, 16, Tile.standardGround)
 
     w.setTileAt(1, 1, Tile.impassableGround)
@@ -32,22 +28,26 @@ class GameScreen extends Screen {
   }
 
   def show() = {}
+
   def hide() = {}
+
   def pause() = {}
+
   def resume() = {}
+
   def dispose() = {}
 
   var t = 0.0
   var simulationAccu = 0.0
   val simulationTickEvery = 1.0 // every 1 second
 
-  def render(delta:Float) {
+  def render(delta: Float) {
     t += delta
     simulationAccu += delta
     // determine game ticks here
     inputHandler.handleFor(world)
 
-    if(simulationAccu >= simulationTickEvery) {
+    if (simulationAccu >= simulationTickEvery) {
       GameLogic.updateWorld(world)
       world.tick += 1
       simulationAccu -= simulationTickEvery
@@ -56,7 +56,7 @@ class GameScreen extends Screen {
     // handle input ... modifies world
     renderer.render(world)
   }
-  def resize(width:Int, height:Int) {
 
+  def resize(width: Int, height: Int) {
   }
 }
