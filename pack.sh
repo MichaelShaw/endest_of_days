049879b482225e39210585eaa463bdfa1b166a8d
Michael Shaw <michaeljshaw@gmail.com>
2016-07-07 15:03:36 +1000

    Update

--- pack.sh
+++ pack.sh
@@ -5,23 +5,24 @@ java -jar packr.jar \
      -platform mac \
      -jdk "../openjdk-1.7.0-u45-unofficial-icedtea-2.4.3-macosx-x86_64-image.zip" \
      -executable endest_of_days \
-     -appjar desktop/target/hack-0.1.jar \
-     -mainclass "hack/Main" \
+     -classpath desktop/target/hack-0.1.jar \
+     -mainclass "hack.Main" \
      -vmargs "-Xmx1G" \
      -minimizejre "soft" \
+     -bundleidentifier end.of.days \
      -outdir endest_of_days.app
-zip -r endest_of_days.mac.zip endest_of_days.app
-echo "======= Packing for windows i586 ======="
-java -jar packr.jar \
-     -platform windows \
-     -jdk "../openjdk-1.7.0-u45-unofficial-icedtea-2.4.3-windows-i586-image.zip" \
-     -executable endest_of_days \
-     -appjar desktop/target/hack-0.1.jar \
-     -mainclass "hack/Main" \
-     -vmargs "-Xmx1G" \
-     -minimizejre "soft" \
-     -outdir endest_of_days
-zip -r endest_of_days.win.zip endest_of_days
+# zip -r endest_of_days.mac.zip endest_of_days.app
+# echo "======= Packing for windows i586 ======="
+# java -jar packr.jar \
+#      -platform windows \
+#      -jdk "../openjdk-1.7.0-u45-unofficial-icedtea-2.4.3-windows-i586-image.zip" \
+#      -executable endest_of_days \
+#      -appjar desktop/target/hack-0.1.jar \
+#      -mainclass "hack/Main" \
+#      -vmargs "-Xmx1G" \
+#      -minimizejre "soft" \
+#      -outdir endest_of_days
+# zip -r endest_of_days.win.zip endest_of_days
 #      -jdk "../openjdk-1.7.0-u60-unofficial-macosx-x86_64-image.zip" \
 #      -jdk "../openjdk-1.7.0-u45-unofficial-icedtea-2.4.3-macosx-x86_64-image.zip" \
 #     -resources pom.xml;src/main/resources \
